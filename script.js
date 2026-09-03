a9fcd40867a3eb10bccfe2144ac1999aba623981
Irrad G <irradganal@student.laverdad.edu.ph>
2026-09-03 21:12:33 +0800

    Second Commit - Added Form Submission and Modified some codes

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const NameInput = document.getElementById('nameInput');
-const WelcomeMessage = document.getElementById('welcomeMessage');
+const NameInput = document.getElementById("nameInput");
+const WelcomeMessage = document.getElementById("welcomeMessage");
+
+const studentForm = document.getElementById("studentForm");
+const studentCourse = document.getElementById("courseInput");
 
 
 NameInput.addEventListener('input', function() {
 
     WelcomeMessage.textContent = "Welcome, " + NameInput.value + "!";
 });
+
+
+
+studentForm.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    const name = NameInput.value;
+    const course = studentCourse.value;
+
+    WelcomeMessage.textContent = "Registration successful! Name:" + name + ", Course: " + course;
+});
+
